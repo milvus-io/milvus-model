d58645936a3b2fc1862636ec88c9ee7f49c191a9
Joan Martinez <joan.fontanals.martinez@jina.ai>
2024-05-08 11:49:38 +0200

    feat: add jinaai embedding and reranker models

--- milvus_model/dense/__init__.py
+++ milvus_model/dense/__init__.py
@@ -1,9 +1,11 @@
 from .openai import OpenAIEmbeddingFunction
 from .sentence_transformer import SentenceTransformerEmbeddingFunction
 from .voyageai import VoyageEmbeddingFunction
+from .jinaai import JinaEmbeddingFunction
 
 __all__ = [
     "OpenAIEmbeddingFunction",
     "SentenceTransformerEmbeddingFunction",
     "VoyageEmbeddingFunction",
+    "JinaEmbeddingFunction",
 ]

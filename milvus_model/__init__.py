60de70761eaa0ba6e1326210760e6e5d6698ec5d
wxywb <xy.wang@zilliz.com>
2024-05-10 19:02:29 +0800

    fix: Import reranker in top-level of package.
    Signed-off-by: wxywb <xy.wang@zilliz.com>

--- milvus_model/__init__.py
+++ milvus_model/__init__.py
@@ -1,6 +1,6 @@
 from . import dense, hybrid, sparse
 from .dense.sentence_transformer import SentenceTransformerEmbeddingFunction
 
-__all__ = ["DefaultEmbeddingFunction", "dense", "sparse", "hybrid"]
+__all__ = ["DefaultEmbeddingFunction", "dense", "sparse", "hybrid", "reranker"]
 
 DefaultEmbeddingFunction = SentenceTransformerEmbeddingFunction

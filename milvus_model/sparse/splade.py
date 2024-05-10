315a232cf24406b7a4598be05351d9229fe539f5
wxywb <xy.wang@zilliz.com>
2024-05-10 19:28:24 +0800

    fix: move no_grad from _encode to forward.
    Signed-off-by: wxywb <xy.wang@zilliz.com>

--- milvus_model/sparse/splade.py
+++ milvus_model/sparse/splade.py
@@ -128,26 +128,26 @@ class _SpladeImplementation:
             padding=True,
         )
         encoded_input = {key: val.to(self.device) for key, val in encoded_input.items()}
-        with torch.no_grad():
-            output = self.model(**encoded_input)
+        output = self.model(**encoded_input)
         return output.logits
 
     def _batchify(self, texts: List[str], batch_size: int) -> List[List[str]]:
         return [texts[i : i + batch_size] for i in range(0, len(texts), batch_size)]
 
     def forward(self, texts: List[str], k_tokens: int) -> csr_array:
-        batched_texts = self._batchify(texts, self.batch_size)
-        sparse_embs = []
-        for batch_texts in batched_texts:
-            logits = self._encode(texts=batch_texts)
-            activations = self._get_activation(logits=logits)
-            if k_tokens is None:
-                nonzero_indices = torch.nonzero(activations["sparse_activations"])
-                activations["activations"] = nonzero_indices
-            else:
-                activations = self._update_activations(**activations, k_tokens=k_tokens)
-            batch_csr = self._convert_to_csr_array(activations)
-            sparse_embs.extend(batch_csr)
+        with torch.no_grad():
+            batched_texts = self._batchify(texts, self.batch_size)
+            sparse_embs = []
+            for batch_texts in batched_texts:
+                logits = self._encode(texts=batch_texts)
+                activations = self._get_activation(logits=logits)
+                if k_tokens is None:
+                    nonzero_indices = torch.nonzero(activations["sparse_activations"])
+                    activations["activations"] = nonzero_indices
+                else:
+                    activations = self._update_activations(**activations, k_tokens=k_tokens)
+                batch_csr = self._convert_to_csr_array(activations)
+                sparse_embs.extend(batch_csr)
 
         return vstack(sparse_embs).tocsr()
 

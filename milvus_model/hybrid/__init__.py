86f7f1dedbfbb5a40366a1895ef0b9a85f00b69d
wxywb <xy.wang@zilliz.com>
2024-08-06 17:26:31 +0800

    feat: Add mGTE embedding function.
    Signed-off-by: wxywb <xy.wang@zilliz.com>

--- milvus_model/hybrid/__init__.py
+++ milvus_model/hybrid/__init__.py
@@ -1,8 +1,12 @@
-__all__ = ["BGEM3EmbeddingFunction"]
+__all__ = ["BGEM3EmbeddingFunction", "MGTEEmbeddingFunction"]
 
 from milvus_model.utils.lazy_import import LazyImport
 
 bge_m3 = LazyImport("bge_m3", globals(), "milvus_model.hybrid.bge_m3")
+mgte = LazyImport("mgte", globals(), "milvus_model.hybrid.mgte")
 
 def BGEM3EmbeddingFunction(*args, **kwargs):
     return bge_m3.BGEM3EmbeddingFunction(*args, **kwargs)
+
+def MGTEEmbeddingFunction(*args, **kwargs):
+    return mgte.MGTEEmbeddingFunction(*args, **kwargs)

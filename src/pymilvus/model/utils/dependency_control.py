23f94d5006d818a5e63bfbfd3f54191231dcbab8
Xiangzhuang Shen <datahonor@gmail.com>
2025-12-15 16:37:46 +0800

    chore(log): add pip install log

--- src/pymilvus/model/utils/dependency_control.py
+++ src/pymilvus/model/utils/dependency_control.py
@@ -3,6 +3,8 @@ import subprocess
 def prompt_install(package: str, warn: bool = False):  # pragma: no cover
     cmd = f"pip install -q {package}"
     try:
+        print(f"milvus-model will install {package} with `{cmd}`.\n"
+              f"Note: If you don't use pip to manage your environment, please install {package} manually.")
         if warn and input(f"Install {package}? Y/n: ") != "Y":
             raise ModuleNotFoundError(f"No module named {package}")
         print(f"start to install package: {package}")
